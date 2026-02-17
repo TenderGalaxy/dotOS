58eec4117905ee185cb00b03c4ed85762372fcb6
fenl <145521130+TenderGalaxy@users.noreply.github.com>
2026-02-17 15:17:26 -0800

    Add dotError module for error handling

---
+++ src/modules/dotError.js
@@ -0,0 +1,39 @@
+{
+  info: {
+    name: 'dotError',
+    type: 'worldcode',
+    source: 'github.com/tendergalaxy/dotOS/blob/main/src/modules/dotError.js',
+    requirements: []
+  },
+  callbacks: {
+    onLoad(){
+      globalThis.dotError = {
+      	try(code, ...args){
+      		dotError.point = ''
+      		dotError.src = code.split(/[\n;]+/)
+      		try{
+      			Function(code)(args)
+      		} catch(e){
+      			dotError.e = e
+      			dotError.point = +e.stack.split(' ')[6].slice(9).replace(')\n', '') - 3
+      		}
+      	},
+      	log(){
+      		if(dotError.point === ''){
+      			api.broadcastMessage('No Errors Found!', {color: 'red'})
+      			return
+      		}
+      		let m = dotError.e.stack.split('\n')
+      		m = Array.from(m, (x) => x.slice(4)).slice(0, m.length - 4)
+      		api.broadcastMessage([{str: `Line ${dotError.point}: ${dotError.e.message}\n`, style: {color: 'orange'}},
+      		{str: `>| ${dotError.src[dotError.point]}\n`, style: {color: 'lightblue'}},
+      		{str: m.join('\n'), style: {color: 'orange'}}
+      ])
+      	}
+      	hasError(){
+      		return (dotError.point !== '')
+      	}
+      }
+    }
+  }
+}
