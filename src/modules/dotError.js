c840786c7439bee57e85a252d7a8d8402bbc7234
fenl <145521130+TenderGalaxy@users.noreply.github.com>
2026-02-21 10:26:58 -0800

    Update dotError.js

--- src/modules/dotError.js
+++ src/modules/dotError.js
@@ -1,3 +1,41 @@
+class dotErr {
+  constructor(){}
+  try(code, ...args){
+    this.point = ''
+    this.src = code.split(/[\n;]+/)
+    try{
+    	return new Function(code)(...args)
+    } catch(e){
+    	this.e = e
+    	this.point = +e.stack.split(' ')[6].slice(9).replace(')\n', '') - 3
+    }
+  }
+  tryFunction(code, ...args){
+    dotError.point = ''
+    dotError.src = code.toString().split(/[\n;]+/)
+    try{
+    	return (code)(...args)
+    } catch(e){
+    	dotError.e = e
+    	dotError.point = +e.stack.split(' ')[6].slice(9).replace(')\n', '') + 1
+    }
+  }
+  log(){
+  	if(dotError.point === ''){
+  		api.broadcastMessage('No Errors Found!', {color: 'red'})
+  		return
+  	}
+  	let m = dotError.e.stack.split('\n')
+  	m = Array.from(m, (x) => x.slice(4)).slice(0, m.length - 4)
+  	api.broadcastMessage([{str: `Line ${dotError.point}: ${dotError.e.message}\n`, style: {color: 'orange'}},
+  	{str: `>| ${dotError.src[dotError.point]}\n`, style: {color: 'lightblue'}},
+  	{str: m.join('\n'), style: {color: 'orange'}}])
+  },
+  hasError(){
+  	return dotError.point !== ''
+  }
+}
+
 let otp = {
   info: {
     name: 'dotError',
@@ -8,42 +46,7 @@ let otp = {
   },
   callbacks: {
     onLoad(){
-      globalThis.dotError = {
-      	try(code, ...args){
-      		dotError.point = ''
-      		dotError.src = code.split(/[\n;]+/)
-      		try{
-      			return new Function(code)(...args)
-      		} catch(e){
-      			dotError.e = e
-      			dotError.point = +e.stack.split(' ')[6].slice(9).replace(')\n', '') - 3
-      		}
-      	},
-        tryFunction(code, ...args){
-          dotError.point = ''
-      		dotError.src = code.toString().split(/[\n;]+/)
-      		try{
-      			return (code)(...args)
-      		} catch(e){
-      			dotError.e = e
-      			dotError.point = +e.stack.split(' ')[6].slice(9).replace(')\n', '') + 1
-      		}
-        },
-      	log(){
-      		if(dotError.point === ''){
-      			api.broadcastMessage('No Errors Found!', {color: 'red'})
-      			return
-      		}
-      		let m = dotError.e.stack.split('\n')
-      		m = Array.from(m, (x) => x.slice(4)).slice(0, m.length - 4)
-      		api.broadcastMessage([{str: `Line ${dotError.point}: ${dotError.e.message}\n`, style: {color: 'orange'}},
-      		{str: `>| ${dotError.src[dotError.point]}\n`, style: {color: 'lightblue'}},
-      		{str: m.join('\n'), style: {color: 'orange'}}])
-      	},
-      	hasError(){
-      		return dotError.point !== ''
-      	}
-      }
+      globalThis.dotError = new dotErr()
     }
   }
 }; otp
