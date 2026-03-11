9dee14d86fe48e7f456516bfa71c3003f4abc4a9
fenlAtSchool <alechan32@nuevaschool.org>
2026-03-11 10:06:55 -0700

    Update userTracker.js

--- src/modules/userTracker.js
+++ src/modules/userTracker.js
@@ -8,12 +8,27 @@
   },
   callbacks: {
     onLoad(){
-      dotOS.user = {id: myId, pos: []}
+      dotOS.user = {
+        id: myId,
+        pos: [],
+        cam: [1, 0, 0]
+      }
+      api.setCameraDirection(myId, [1,0,0])
       globalThis.dotOS.updateUserPositions = function(){
         globalThis.dotOS.user.pos = api.getPosition(dotOS.user.id)
-        TS.setTimeout(dotOS.updateUserPositions, 2)
       }
-      dotOS.updateUserPositions()
+      globalThis.dotOS.updateUserCam = function(){
+        let add = (a, b) => {
+          return [a[0] + b[0], a[1] + b[1], a[2] + b[2]]
+        }
+        dotOS.user.cam = add(dotOS.user.cam, api.getPlayerFacingInfo(dotOS.user.id).dir)
+      }
+      globalThis.dotOS.handleUserInput = function(){
+        dotOS.updateUserPositions()
+        dotOS.updateUserCam()
+        TS.setTimeout(dotOS.handleUserInput, 2)
+      }
+      dotOS.handleUserInput()
     }
   }
 }
