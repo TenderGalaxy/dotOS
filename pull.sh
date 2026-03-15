b228540777d4dcaff17368397ecef95edc08e54e
fenl <145521130+TenderGalaxy@users.noreply.github.com>
2026-03-14 20:28:25 -0700

    Update pull.sh

--- pull.sh
+++ pull.sh
@@ -1,7 +1,16 @@
 #!/bin/zsh
 cd "$(dirname "$0")" || exit
 echo "Updating repo..."
-git reset --hard origin/main
+git reset --hard origin/main || {
+  echo "Install git!"
+  exit
+}
 echo "Building dotOS…"
-pip install -r requirements.txt
-python3 build.py
+pip install -r requirements.txt || {
+  echo "Install python!"
+  exit
+}
+python3 build.py || {
+  echo "Wtf you installed pip but not python?"
+  exit
+}
