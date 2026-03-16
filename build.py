c056a77d1518588ec2cb5e5ba2e70ebe57b6c982
fenl <145521130+TenderGalaxy@users.noreply.github.com>
2026-03-15 20:31:47 -0700

    Minor fix

--- build.py
+++ build.py
@@ -88,7 +88,7 @@ for fileName in dir:
     '''f = f.replace('"', '\\"')
     f = f.replace("'", "\\'")
     f = f.replace('`', '\\`')'''
-    obj = js.eval(f)
+    js.eval(f)
     objtype = js.eval('obj.info.type')
     js.eval('callbackNames = Object.keys(obj.callbacks)')
     req = getList('obj.info.requirements')
